1ccfe550db21e949ee2ae31eac09eaf58bac7c65
王穎乾 <charly11charly@gmail.com>
2024-07-14 07:41:05 +0000

    modify

--- linebot_test/main.py
+++ linebot_test/main.py
@@ -14,7 +14,7 @@ handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
 @app.route("/")
 def index():
-    return "<h1>我的第一個網站--測試</h1>"
+    return "<h1>LineBot的webhook程式</h1>"
 
 @app.route("/callback",methods=['POST'])
 def callback():
